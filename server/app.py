cedb7e1f852494b48a490074738333f76cf966bd
Jessica Vaughn <jessica.vaughn619@gmail.com>
2023-05-15 16:55:07 -0600

    Initial commit

---
+++ server/app.py
@@ -0,0 +1,17 @@
+import os
+
+from flask import Flask, make_response, jsonify
+from flask_migrate import Migrate
+from flask_restful import Api, Resource
+
+from models import db
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URI')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.json.compact = False
+
+migrate = Migrate(app, db)
+db.init_app(app)
+
+api = Api(app)
