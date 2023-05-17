e07bc40d183656b5b3c7ee55e62e031aee30faeb
Jessica Vaughn <jessica.vaughn619@gmail.com>
2023-05-17 14:32:13 -0600

    Add signup component and connect front and backend using honcho

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
+import React, { useEffect } from 'react';
 import './App.css';
+import { Signup } from './components/Signup';
+
 
 function App() {
+  useEffect(() => {
+    fetch("/signup")
+    .then((r) => r.json())
+  }, [])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Signup />
     </div>
-  );
+  )
 }
 
 export default App;
