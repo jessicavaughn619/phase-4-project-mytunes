db8f0438ba6213e5c9491b3e6b98cb6d410dcc3a
Jessica Vaughn <jessica.vaughn619@gmail.com>
2023-05-18 09:34:46 -0600

    Add to requirements

--- server/config.py
+++ server/config.py
@@ -1,13 +1,13 @@
-from dotenv import load_dotenv
-load_dotenv()
-
 import os
+from dotenv import load_dotenv
 from flask import Flask
 from flask_bcrypt import Bcrypt
 from flask_migrate import Migrate
 from flask_restful import Api
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import MetaData
+
+load_dotenv()
 
 app = Flask(
     __name__,
