81ce683cddd573c61e223185c1826831e7403415
Jessica Vaughn <jessica.vaughn619@gmail.com>
2023-05-18 12:20:06 -0600

    Add login form and navbar, update signup form, connect from and back end port

---
+++ client/src/components/SignUpForm.js
@@ -0,0 +1,81 @@
+import React, { useState } from "react";
+
+function SignUpForm({ onLogin }) {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [passwordConfirmation, setPasswordConfirmation] = useState("");
+  const [imageUrl, setImageUrl] = useState("");
+  const [errors, setErrors] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    setErrors([]);
+    setIsLoading(true);
+    fetch("/signup", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        username,
+        password,
+        password_confirmation: passwordConfirmation,
+        image_url: imageUrl,
+      }),
+    }).then((r) => {
+      setIsLoading(false);
+      if (r.ok) {
+        r.json().then((user) => onLogin(user));
+      } else {
+        r.json().then((err) => setErrors(err.errors));
+      }
+    });
+  }
+
+  return (
+    <form onSubmit={handleSubmit}>
+        <label htmlFor="username">Username</label>
+        <input
+          type="text"
+          id="username"
+          autoComplete="off"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+        />
+        <br></br>
+        <label htmlFor="password">Password</label>
+        <input
+          type="password"
+          id="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          autoComplete="current-password"
+        />
+        <br></br>
+        <label htmlFor="password">Password Confirmation</label>
+        <input
+          type="password"
+          id="password_confirmation"
+          value={passwordConfirmation}
+          onChange={(e) => setPasswordConfirmation(e.target.value)}
+          autoComplete="current-password"
+        />
+        <br></br>
+        <label htmlFor="imageUrl">Profile Image</label>
+        <input
+          type="text"
+          id="imageUrl"
+          value={imageUrl}
+          onChange={(e) => setImageUrl(e.target.value)}
+        />
+        <br></br>
+        <button type="submit">{isLoading ? "Loading..." : "Sign Up"}</button>
+        {errors.map((err) => (
+          console.log(err)
+        ))}
+    </form>
+  );
+}
+
+export default SignUpForm;
--- server/config.py
+++ server/config.py
@@ -13,6 +13,7 @@ app = Flask(
     static_url_path='',
     static_folder='../client/build',
     template_folder='../client/build')
+app.secret_key = os.urandom(24)
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URI')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.json.compact = False
