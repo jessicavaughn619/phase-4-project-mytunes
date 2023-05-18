209f01d572b6d8fd1ec635150bbf6ae4c1e5dca3
Jessica Vaughn <jessica.vaughn619@gmail.com>
2023-05-18 09:17:43 -0600

    Update client/Signup.js to function and allow deploy

--- client/src/components/Signup.js
+++ client/src/components/Signup.js
@@ -1,20 +1,52 @@
 import React, { useEffect, useState } from "react";
-// import { useFormik } from "formik";
-// import * as yup from "yup";
+import { useFormik } from "formik";
+import * as yup from "yup";
 export const Signup = () => {
     const [users, setUsers] = useState([{}]);
+    const [refreshPage, setRefreshPage] = useState(false);
 
     useEffect(() => {
         fetch("/users")
         .then((r) => r.json())
         .then((data) => setUsers(data))
-    })
+    }, [refreshPage]);
+
+    console.log(users)
+
+    const formSchema = yup.object().shape({
+        username: yup.string().required("Must enter a username")
+    });
+
+    const formik = useFormik({
+        initialValues: {
+            username: "",
+        },
+        validationSchema: formSchema,
+        onSubmit: (values) => {
+            fetch("users", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify(values, null, 2),
+            }).then((r) => {
+                if (r.status == 200) {
+                    setRefreshPage(!refreshPage);
+                }
+            });
+        },
+    });
+
     return (
         <div>
             <h1>Sign Up for MyTunes</h1>
-            <form>
+            <form onSubmit={formik.handleSubmit}>
                 <input
-                id="username"></input>
+                id="username"
+                name="username"
+                onChange={formik.handleChange}
+                value={formik.values.username}></input>
+                <button type="submit">Submit</button>
             </form>
         </div>
     )
