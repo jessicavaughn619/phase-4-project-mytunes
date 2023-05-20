2523da925cb48521cb96609dd9a3c280f574927f
Jessica Vaughn <jessica.vaughn619@gmail.com>
2023-05-20 15:01:13 -0600

    Add const for myPlaylists to filter current session user playlists

--- client/src/components/Playlists.js
+++ client/src/components/Playlists.js
@@ -1,16 +1,19 @@
 import React from 'react';
 import Playlist from './Playlist';
 
-const Playlists = ({ users, playlists }) => {
+const Playlists = ({ user, playlists }) => {
     const allPlaylists = playlists.map((playlist) => (
         <Playlist 
         key={playlist.id}
         playlist={playlist}/>
     ))
-console.log(users)
+
+    const myPlaylists = playlists.filter((playlist) => playlist.user_id === user.id);
 
   return (
     <div id="playlists-container-wrapper">
+        <h2>My Playlists</h2>
+        {myPlaylists}
         <h2>All Playlists</h2>
         {allPlaylists}
     </div>
