66dcda5b22780252612f01c8267accef080a8d66
Jessica Vaughn <jessica.vaughn619@gmail.com>
2023-05-22 09:36:41 -0600

    Add Swiper to UserList component

--- client/src/components/UserList.js
+++ client/src/components/UserList.js
@@ -1,18 +1,33 @@
 import React from "react";
 import UserCard from "./UserCard";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Navigation, Pagination, Scrollbar, A11y } from 'swiper';
 import "../stylesheets/UserList.scss";
+import 'swiper/scss';
+import 'swiper/scss/navigation';
+import 'swiper/scss/pagination';
+import 'swiper/scss/scrollbar';
 
 function UserList({ users }) {
 
     const allUsers = users.map((user) => (
-        <UserCard 
+        <SwiperSlide>
+            <UserCard
             key={user.id}
-            user={user}
-        /> ))
+            user={user} />
+        </SwiperSlide>))
     return (
         <div id="userlist-container-wrapper">
         <h2>Users</h2>
+        <Swiper
+        modules={[Navigation, Pagination, Scrollbar, A11y]}
+        spaceBetween={50}
+        slidesPerView={3}
+        navigation
+        pagination={{clickable: true }}
+        scrollbar={{draggable: true }}>
             {allUsers}
+        </Swiper>
         </div>
     )
 }
