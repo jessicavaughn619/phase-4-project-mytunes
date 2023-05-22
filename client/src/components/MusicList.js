48d9029e569824fcdfe01a52e42878af6dd6d878
Jessica Vaughn <jessica.vaughn619@gmail.com>
2023-05-22 09:46:14 -0600

    Add Swiper to MusicList component, style UserList, MusicList, PlayLists components

--- client/src/components/MusicList.js
+++ client/src/components/MusicList.js
@@ -1,19 +1,35 @@
 import React from 'react';
 import ArtistCard from './ArtistCard';
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Navigation, Pagination, Scrollbar, A11y } from 'swiper';
 import "../stylesheets/MusicList.scss";
+import 'swiper/scss';
+import 'swiper/scss/navigation';
+import 'swiper/scss/pagination';
+import 'swiper/scss/scrollbar';
 
 const MusicList = ({ artists }) => {
 
   const allArtists = artists.map((artist) => (
-    <ArtistCard
-        key={artist.id}
-        artist={artist}
-    /> ))
+    <SwiperSlide>
+      <ArtistCard
+          key={artist.id}
+          artist={artist}
+      />
+    </SwiperSlide>))
 
   return (
     <div id="musiclist-container-wrapper">
         <h2>Artists</h2>
-        {allArtists}
+        <Swiper
+        modules={[Navigation, Pagination, Scrollbar, A11y]}
+        spaceBetween={50}
+        slidesPerView={3}
+        navigation
+        pagination={{clickable: true }}
+        scrollbar={{draggable: true }}>
+            {allArtists}
+        </Swiper>
     </div>
   )
 }
