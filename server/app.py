517b9a33c57f4416bbfc7d8bed79577020a1403b
Jessica Vaughn <jessica.vaughn619@gmail.com>
2023-06-08 05:04:29 -0600

    Add post method to Music (artist) resource in app.py

--- server/app.py
+++ server/app.py
@@ -14,17 +14,40 @@ def index():
 class Users(Resource):
     def get(self):
         users = [user.to_dict() for user in User.query.all()]
-        return make_response(jsonify(users), 200)
+        return make_response(users, 200)  
     
 class Music(Resource):
     def get(self):
         music = [artist.to_dict() for artist in Artist.query.all()]
-        return make_response(jsonify(music), 200)
+        return make_response(music, 200)
+    
+    def post(self):
+        request_json = request.get_json()
+
+        name = request_json.get('name')
+        spotify_id= request_json.get('spotifyId')
+        image_url = request_json.get('imageUrl')
+        genres = request_json.get('genres')
+
+        artist = Artist(
+            name=name,
+            spotify_id=spotify_id,
+            image_url=image_url,
+            genres=genres,
+        )
+
+        try:
+            db.session.add(artist)
+            db.session.commit()
+            return {'message' : 'Artist successfully added to database'}, 201
+        
+        except IntegrityError:
+            return {'error': '422 Unprocessable entity'}, 422
     
 class Playlists(Resource):
     def get(self):
         playlists = [playlist.to_dict() for playlist in Playlist.query.all()]
-        return make_response(jsonify(playlists), 200)
+        return make_response(playlists, 200)
     
     def post(self):
         request_json = request.get_json()
@@ -46,7 +69,7 @@ class Playlists(Resource):
 class PlaylistByID(Resource):
     def get(self, id):
         playlist = Playlist.query.filter_by(id=id).first().to_dict()
-        return make_response(jsonify(playlist), 200)
+        return make_response(playlist, 200)
     
     def patch(self, id):
         playlist = Playlist.query.filter_by(id=id).first()
